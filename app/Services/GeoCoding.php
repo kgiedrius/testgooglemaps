bedbfc4b62d7d2060d5c24b2676356278e7e74d9
giedrius <kgiedrius@gmail.com>
2017-07-25 17:04:31 +0300

    finished task

---
+++ app/Services/GeoCoding.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Services;
+
+class GeoCoding
+{
+    public function getLocationsDetails(array $locations)
+    {
+    }
+
+    public function getShortestRoute(array $locations)
+    {
+    }
+}
